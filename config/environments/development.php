1ca9eb03ba97a2cd66aff845b311977ebdcfae75
chriszarate <chris@zarate.org>
2015-06-13 15:19:20 -0400

    Turn on debug log by default, alongside nginx logs.

--- config/environments/development.php
+++ config/environments/development.php
@@ -1,8 +1,13 @@
 <?php
 /* Development */
-define('SAVEQUERIES', true);
+
+/* Debug log */
 define('WP_DEBUG', true);
+define('WP_DEBUG_DISPLAY', false);
+define('SAVEQUERIES', true);
 define('SCRIPT_DEBUG', true);
+ini_set('log_errors', 1);
+ini_set('error_log', getenv('WP_LOGS_DIR') . '/debug.log');
 
 /* Changes to vanilla Bedrock below this line */
 
