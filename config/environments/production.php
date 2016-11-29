7f2d4ac42c9d7403a7213bac35b546ed06baf19e
Ryan Williams <ryrowi@gmail.com>
2016-11-29 10:03:07 -0500

    add SUNRISE globals to production env config

--- config/environments/production.php
+++ config/environments/production.php
@@ -4,3 +4,6 @@ ini_set('display_errors', 0);
 define('WP_DEBUG_DISPLAY', false);
 define('SCRIPT_DEBUG', false);
 define('DISALLOW_FILE_MODS', true); // this disables all file modifications including updates and update notifications
+
+define('SUNRISE', 'on');
+define('PLUGINDIR', 'app/plugins');
