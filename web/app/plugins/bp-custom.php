63f53410eb753abee107685b12bc62af91e2f711
Ryan Williams <ryrowi@gmail.com>
2016-12-06 15:48:52 -0500

    trello ke7BsWmN: add mla group types/status filters

--- web/app/plugins/bp-custom.php
+++ web/app/plugins/bp-custom.php
@@ -74,3 +74,180 @@ function hcommons_filter_get_followers( $followers ) {
 	return $followers;
 }
 add_filter( 'bp_follow_get_followers', 'hcommons_filter_get_followers' );
+
+
+
+
+
+
+
+class MLA_Groups {
+
+	function __construct() {
+		add_action( 'bp_groups_directory_group_types', [ $this, 'add_type_filter' ] );
+		add_action( 'bp_groups_directory_group_types', [ $this, 'add_status_filter' ] );
+
+		add_action( 'wp_footer', [ $this, 'type_filter_js' ] );
+		add_action( 'wp_footer', [ $this, 'status_filter_js' ] );
+
+		add_filter( 'bp_before_has_groups_parse_args', [ $this, 'filter_bp_before_has_groups_parse_args' ] );
+		add_filter( 'bp_groups_get_paged_groups_sql', [ $this, 'filter_bp_groups_get_paged_groups_sql' ], null, 3 );
+	}
+
+	function add_status_filter() {
+		$str  = '<li class="filter-status">';
+		$str .= '<select id="groups-filter-by-status">';
+		$str .= '<option value="all">All Visibilities</option>';
+		$str .= '<option value="public">Public</option>';
+		$str .= '<option value="private">Private</option>';
+		if ( is_admin() || is_super_admin() ) {
+			$str .= '<option value="hidden">Hidden</option>';
+		}
+		$str .= '</select></li>';
+		echo $str;
+	}
+
+	function add_type_filter() {
+		$str  = '<li class="filter-type">';
+		$str .= '<select id="groups-filter-by-type">';
+		$str .= '<option value="all">All Types</option>';
+		$str .= '<option value="committees">Committees</option>';
+		$str .= '<option value="forums">Forums</option>';
+		$str .= '<option value="prospective_forums">Prospective Forums</option>';
+		$str .= '<option value="other">Other</option>';
+		$str .= '</select></li>';
+		echo $str;
+	}
+
+	function status_filter_js() {
+		if ( wp_script_is( 'jquery', 'done' ) ) { ?>
+			<script type="text/javascript">
+				if (jq.cookie('bp-groups-status')) {
+					jq('li.filter-status select').val(jq.cookie('bp-groups-status'));
+			}
+			jq('li.filter-status select').change( function() {
+
+				if ( jq('.item-list-tabs li.selected').length )
+					var el = jq('.item-list-tabs li.selected');
+				else
+					var el = jq(this);
+
+				var css_id = el.attr('id').split('-');
+				var object = css_id[0];
+				var scope = css_id[1];
+				var status = jq(this).val();
+				var filter = jq('select#groups-order-by').val();
+				var search_terms = '';
+
+				jq.cookie('bp-groups-status',status,{ path: '/' });
+
+				if ( jq('.dir-search input').length )
+					search_terms = jq('.dir-search input').val();
+
+				bp_filter_request( object, filter, scope, 'div.' + object, search_terms, 1, jq.cookie('bp-' + object + '-extras') );
+
+				return false;
+
+			});
+			</script>
+		<?php }
+	}
+
+	function type_filter_js() {
+		if ( wp_script_is( 'jquery', 'done' ) ) { ?>
+		<script>
+			if (jq.cookie('bp-groups-status')) {
+				jq('li.filter-type select').val(jq.cookie('bp-groups-type'));
+			}
+			jq('li.filter-type select').change( function() {
+
+				if ( jq('.item-list-tabs li.selected').length )
+					var el = jq('.item-list-tabs li.selected');
+				else
+					var el = jq(this);
+
+				var css_id = el.attr('id').split('-');
+				var object = css_id[0];
+				var scope = css_id[1];
+				var status = jq(this).val();
+				var filter = jq('select#groups-order-by-type').val();
+				var search_terms = '';
+
+				jq.cookie('bp-groups-type',status,{ path: '/' });
+
+				if ( jq('.dir-search input').length )
+					search_terms = jq('.dir-search input').val();
+
+				bp_filter_request( object, filter, scope, 'div.' + object, search_terms, 1, jq.cookie('bp-' + object + '-extras') );
+
+				return false;
+
+			});
+		</script>
+		<?php }
+	}
+
+	function filter_bp_before_has_groups_parse_args( $args ) {
+		$type = $_COOKIE['bp-groups-type'];
+
+		if ( bp_is_groups_directory() && ! empty( $type ) ) {
+			switch ( $type ) {
+			case 'committees':
+				$value = '^M';
+				break;
+			case 'forums':
+				$value = '^(D|G)';
+				break;
+			case 'prospective_forums':
+				$value = '^F';
+				break;
+			}
+
+			if ( ! empty( $value ) ) {
+				if ( empty( $args['meta_query'] ) ) {
+					$args['meta_query'] = [];
+				}
+
+				$args['meta_query'][] = [
+					'key' => 'mla_oid',
+					'value' => $value,
+					'compare' => 'RLIKE',
+				];
+			}
+		}
+
+		return $args;
+	}
+
+	function filter_bp_groups_get_paged_groups_sql( $sql_str, $sql_arr, $r ) {
+		$status = $_COOKIE['bp-groups-status'];
+
+		if ( bp_is_groups_directory() && ! empty( $status ) ) {
+			switch ( $status ) {
+			case 'private':
+			case 'public':
+				$value = $status;
+				break;
+			case 'hidden':
+				if ( is_admin() || is_super_admin() ) {
+					$value = $status;
+				}
+				break;
+			}
+
+			if ( ! empty( $value ) ) {
+				$sql_arr['hidden'] = " AND g.status = '$value'";
+			}
+		}
+
+		return join( ' ', (array) $sql_arr );
+	}
+
+}
+
+function hcommons_init_mla_groups() {
+	if ( class_exists( 'Humanities_Commons' ) && 'mla' === Humanities_Commons::$society_id ) {
+		$MLA_Groups = new MLA_Groups;
+	}
+}
+add_action( 'bp_init', 'hcommons_init_mla_groups' );
