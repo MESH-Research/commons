1f1b288351b1f3a88dc4786c4af790427cafaf5b
Ryan Williams <ryrowi@gmail.com>
2016-12-19 16:51:10 -0500

    trello ToYk6oug: add footer text to all sites telling users about HC

--- web/app/plugins/bp-custom.php
+++ web/app/plugins/bp-custom.php
@@ -82,6 +82,63 @@ function hcommons_filter_groups_activity_new_update_action( $activity_action ) {
 add_filter( 'groups_activity_new_update_action', 'hcommons_filter_groups_activity_new_update_action' );
 
 
+/**
+ * append some text to the bottom of any/all themes to tell users about HC and its networks
+ */
+function hcommons_wp_footer() {
+	function is_society_blog() {
+		$society_blog_ids = [
+			constant( 'HC_ROOT_BLOG_ID' ),
+			constant( 'AJS_ROOT_BLOG_ID' ),
+			constant( 'ASEEES_ROOT_BLOG_ID' ),
+			constant( 'CAA_ROOT_BLOG_ID' ),
+			constant( 'MLA_ROOT_BLOG_ID' ),
+		];
+
+		return in_array( (string) get_current_blog_id(), $society_blog_ids );
+	}
+
+	if (
+		class_exists( 'Humanities_Commons' ) &&
+		! empty( Humanities_Commons::$society_id ) &&
+		! is_society_blog()
+	) {
+		$main_site_domain = Humanities_Commons::$main_site->domain;
+		$society_id = Humanities_Commons::$society_id;
+
+		$society_url = sprintf(
+			'https://%s%s',
+			( 'hc' === $society_id ) ? '' : $society_id . '.',
+			$main_site_domain
+		);
+
+		$style = implode( ';', [
+			'background-color: white',
+			'color: black',
+			'line-height: 3em',
+			'position: relative',
+			'text-align: center',
+			'width: 100%',
+			'z-index: 100',
+		] );
+
+		$text = sprintf(
+			'<div id="hcommons-network-footer" style="%s">This site is part of %s<a href="%s">Humanities Commons</a>. <a href="%s">Explore other sites on this network</a> or <a href="%s">register to build your own</a>.</div>',
+			$style,
+			( 'hc' === $society_id ) ? '' : sprintf( 'the %s network on ', strtoupper( $society_id ) ),
+			'https://' . $main_site_domain,
+			trailingslashit( $society_url ) . 'sites',
+			$society_url
+		);
+
+		// doesn't work too well after all. maybe later.
+		//$script = '<script>jQuery("footer#colophon").append("<br>"+jQuery("#hcommons-network-footer").html());</script>';
+		//echo $text . $script;
+
+		echo $text . $script;
+	}
+}
+add_action( 'wp_footer', 'hcommons_wp_footer' );
 
 
 class MLA_Groups {
