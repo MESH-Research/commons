0d8873b46fbbad5b7181fbdf31853a47d1e1933e
Ryan Williams <ryrowi@gmail.com>
2017-01-03 13:52:12 -0500

    make staging environment config match development

--- config/environments/staging.php
+++ config/environments/staging.php
@@ -1,9 +1,11 @@
 <?php
 /* Staging */
-ini_set('display_errors', 0);
+define('WP_DEBUG', true);
 define('WP_DEBUG_DISPLAY', false);
-define('SCRIPT_DEBUG', false);
-define('DISALLOW_FILE_MODS', true); // this disables all file modifications including updates and update notifications
+define('SAVEQUERIES', true);
+define('SCRIPT_DEBUG', true);
+ini_set('log_errors', 1);
+ini_set('error_log', getenv('WP_LOGS_DIR') . '/debug.log');
 
 /* Changes to vanilla Bedrock below this line */
 
