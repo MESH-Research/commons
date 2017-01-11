5d6381f4152023c62fe44f15474939cfb22d3a1c
Ryan Williams <ryrowi@gmail.com>
2017-01-11 13:14:04 -0500

    enable debug logging in production

--- config/environments/production.php
+++ config/environments/production.php
@@ -5,5 +5,8 @@ define('WP_DEBUG_DISPLAY', false);
 define('SCRIPT_DEBUG', false);
 define('DISALLOW_FILE_MODS', true); // this disables all file modifications including updates and update notifications
 
+ini_set('log_errors', 1);
+ini_set('error_log', getenv('WP_LOGS_DIR') . '/debug.log');
+
 define('SUNRISE', 'on');
 define('PLUGINDIR', 'app/plugins');
