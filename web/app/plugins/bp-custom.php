47ba61e7169a3866c1ccb3129c1d557035407139
Eric Knappe <eknappe@mla.org>
2017-05-08 13:31:49 +0000

    Make activity the default page for groups, add an orcid config setting.

--- web/app/plugins/bp-custom.php
+++ web/app/plugins/bp-custom.php
@@ -509,3 +509,59 @@ function hcommons_unfilter_wp_mail() {
 }
 add_action( 'bbp_pre_notify_subscribers', 'hcommons_unfilter_wp_mail' );
 add_action( 'bbp_pre_notify_forum_subscribers', 'hcommons_unfilter_wp_mail' );
+
+/**
+ * Set the group default tab to 'forum' if the current group has a forum
+ * attached to it.
+ */
+function hcommons_override_cbox_set_group_default_tab( $retval ) {
+        // check if bbPress or legacy forums are active and configured properly
+        if ( ( function_exists( 'bbp_is_group_forums_active' ) && bbp_is_group_forums_active() ) ||
+                ( function_exists( 'bp_forums_is_installed_correctly' ) && bp_forums_is_installed_correctly() ) ) {
+
+                // if current group does not have a forum attached, stop now!
+                if ( ! bp_group_is_forum_enabled( groups_get_current_group() ) ) {
+                        return $retval;
+                }
+
+                // Allow non-logged-in users to view a private group's homepage.
+                if ( false === is_user_logged_in() && groups_get_current_group() && 'private' === bp_get_new_group_status() ) {
+                        return $retval;
+                }
+
+                // reconfigure the group's nav
+                add_action( 'bp_actions', 'hcommons_override_config_group_nav', 99 );
+
+                // finally, use 'forum' as the default group tab
+                return 'home';
+        }
+
+        return $retval;
+}
+add_filter( 'bp_groups_default_extension', 'hcommons_override_cbox_set_group_default_tab', 100 );
+
+/**
+ * On the current group page, reconfigure the group nav when a forum is
+ * enabled for the group.
+ *
+ * What we do here is:
+ *  - move the 'Forum' tab to the beginning of the nav
+ *  - rename the 'Home' tab to 'Activity'
+ */
+function hcommons_override_config_group_nav() {
+        $group_slug = bp_current_item();
+
+        // BP 2.6+.
+        if ( function_exists( 'bp_rest_api_init' ) ) {
+                buddypress()->groups->nav->edit_nav( array( 'position' => 1 ), 'forum', $group_slug );
+                buddypress()->groups->nav->edit_nav( array( 'position' => 0 ), 'home', $group_slug );
+                buddypress()->groups->nav->edit_nav( array( 'name' => __( 'Activity', 'buddypress' ) ), 'home', $group_slug );
+
+        // Older versions of BP.
+        } else {
+                buddypress()->bp_options_nav[$group_slug]['home']['position'] = 0;
+                buddypress()->bp_options_nav[$group_slug]['forum']['position'] = 1;
+                buddypress()->bp_options_nav[$group_slug]['home']['name']      = __( 'Activity', 'buddypress' );
+        }
+
+}
