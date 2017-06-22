9ea8dff95ec8a57d5448df5e2f211142d0ff660a
Ryan Williams <ryrowi@gmail.com>
2017-06-22 10:37:05 -0400

    override wp_verify_nonce() to always verify global superadmins

--- web/app/plugins/bp-custom.php
+++ web/app/plugins/bp-custom.php
@@ -601,6 +601,72 @@ function check_admin_referer( $action = -1, $query_arg = '_wpnonce' ) {
 }
 endif;
 
+// override to allow global super admins to always verify
+function wp_verify_nonce( $nonce, $action = -1 ) {
+	$nonce = (string) $nonce;
+	$user = wp_get_current_user();
+
+	if ( defined( 'GLOBAL_SUPER_ADMINS' ) ) {
+		$global_super_admin_list = constant( 'GLOBAL_SUPER_ADMINS' );
+		$global_super_admins = explode( ',', $global_super_admin_list );
+
+		if (
+			$user &&
+			in_array( $user->user_login, $global_super_admins )
+		) {
+			return 1;
+		}
+	}
+
+	$uid = (int) $user->ID;
+	if ( ! $uid ) {
+		/**
+		 * Filters whether the user who generated the nonce is logged out.
+		 *
+		 * @since 3.5.0
+		 *
+		 * @param int    $uid    ID of the nonce-owning user.
+		 * @param string $action The nonce action.
+		 */
+		$uid = apply_filters( 'nonce_user_logged_out', $uid, $action );
+	}
+
+	if ( empty( $nonce ) ) {
+		return false;
+	}
+
+	$token = wp_get_session_token();
+	$i = wp_nonce_tick();
+
+	// Nonce generated 0-12 hours ago
+	$expected = substr( wp_hash( $i . '|' . $action . '|' . $uid . '|' . $token, 'nonce'), -12, 10 );
+	if ( hash_equals( $expected, $nonce ) ) {
+		return 1;
+	}
+
+	// Nonce generated 12-24 hours ago
+	$expected = substr( wp_hash( ( $i - 1 ) . '|' . $action . '|' . $uid . '|' . $token, 'nonce' ), -12, 10 );
+	if ( hash_equals( $expected, $nonce ) ) {
+		return 2;
+	}
+
+	/**
+	 * Fires when nonce verification fails.
+	 *
+	 * @since 4.4.0
+	 *
+	 * @param string     $nonce  The invalid nonce.
+	 * @param string|int $action The nonce action.
+	 * @param WP_User    $user   The current user object.
+	 * @param string     $token  The user's session token.
+	 */
+	do_action( 'wp_verify_nonce_failed', $nonce, $action, $user, $token );
+
+	// Invalid nonce
+	return false;
+}
+
+
 /**
  * charityhub saves its custom options in a file inside the theme directory.
  * when that happens, filter get_template_dir() to return a writeable dir instead.
