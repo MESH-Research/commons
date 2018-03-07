a0af23c59c161ea3e8afe27265dcd1c723bf8876
Ryan Williams <rwilliams@mla.org>
2018-03-07 14:11:10 -0500

    add disclaimers to headers on member/group dirs for non society member actions

--- web/app/plugins/bp-custom.php
+++ web/app/plugins/bp-custom.php
@@ -34,6 +34,27 @@ function hcommons_add_non_society_member_follow_button() {
 }
 add_action( 'bp_directory_members_actions', 'hcommons_add_non_society_member_follow_button' );
 
+function hcommons_add_non_society_member_disclaimer_member() {
+	if ( hcommons_check_non_member_active_session() ) {
+		printf(
+			'<div class="non-member-disclaimer">Only %s members can follow others from here.<br>To follow these members, go to <a href="%s">Humanities Commons</a>.</div>',
+			strtoupper( Humanities_Commons::$society_id ),
+			get_site_url( getenv( 'HC_ROOT_BLOG_ID' ) )
+		);
+	}
+}
+add_action( 'bp_before_directory_members_content', 'hcommons_add_non_society_member_disclaimer_member' );
+
+function hcommons_add_non_society_member_disclaimer_group() {
+	if ( hcommons_check_non_member_active_session() ) {
+		printf(
+			'<div class="non-member-disclaimer">Only %1$s members can join these groups.<br><a href="/register">Join %1$s now</a>!</div>',
+			strtoupper( Humanities_Commons::$society_id )
+		);
+	}
+}
+add_action( 'bp_before_directory_groups_content', 'hcommons_add_non_society_member_disclaimer_group' );
+
 /**
  * remove urls in comments from comment notification emails so that we don't trigger spam filters
  */
