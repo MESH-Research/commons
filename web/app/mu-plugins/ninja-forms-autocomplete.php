2285b2d1cc114f5e7ac35d4cec0e92956ce3bb37
Ryan Williams <rwilliams@mla.org>
2018-05-30 14:03:53 -0400

    enable browser autocomplete for ninja forms

---
+++ web/app/mu-plugins/ninja-forms-autocomplete.php
@@ -0,0 +1,24 @@
+<?php
+
+function hcommons_ninja_forms_autocomplete() {
+?>
+	<iframe name="ninja-forms-autocomplete" style="display:none" src="https://about:blank"></iframe>
+	<script>
+		jQuery( function( $ ) {
+			var HcNfAutocomplete = Marionette.Object.extend( {
+				initialize: function() {
+					this.listenTo( Backbone.Radio.channel( 'forms' ), 'submit:response', this.actionSubmit );
+				},
+				actionSubmit: function( response ) {
+					$( '.nf-form-wrap form' )
+						.attr( 'target', 'ninja-forms-autocomplete' )
+						.attr( 'action', 'https://example.com' )
+						[0].submit();
+				},
+			});
+			new HcNfAutocomplete();
+		} );
+	</script>
+<?php
+}
+add_action( 'wp_footer', 'hcommons_ninja_forms_autocomplete', 100 );
