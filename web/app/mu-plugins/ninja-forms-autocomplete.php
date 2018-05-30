38b444ebfa2e49caeded150332e131d4e808f112
Ryan Williams <rwilliams@mla.org>
2018-05-30 16:24:05 -0400

    constrain ninja-forms-autocomplete initialization using nf_init hook

--- web/app/mu-plugins/ninja-forms-autocomplete.php
+++ web/app/mu-plugins/ninja-forms-autocomplete.php
@@ -1,24 +1,38 @@
 <?php
+/**
+ * Enable browser autocomplete for ninja forms.
+ *
+ * @package Commons
+ */
 
+/**
+ * Add the ninja forms autocomplete hook to the footer.
+ */
 function hcommons_ninja_forms_autocomplete() {
-?>
-	<iframe name="ninja-forms-autocomplete" style="display:none" src="https://about:blank"></iframe>
-	<script>
+	$print_js = function() {
+		?>
+		<iframe name="ninja-forms-autocomplete" style="display:none" src="https://about:blank"></iframe>
+		<script>
 		jQuery( function( $ ) {
-			var HcNfAutocomplete = Marionette.Object.extend( {
-				initialize: function() {
-					this.listenTo( Backbone.Radio.channel( 'forms' ), 'submit:response', this.actionSubmit );
-				},
-				actionSubmit: function( response ) {
-					$( '.nf-form-wrap form' )
-						.attr( 'target', 'ninja-forms-autocomplete' )
-						.attr( 'action', ajaxurl )
-						[0].submit();
-				},
-			});
-			new HcNfAutocomplete();
+				var HcNfAutocomplete = Marionette.Object.extend( {
+					initialize: function() {
+						this.listenTo( Backbone.Radio.channel( 'forms' ), 'submit:response', this.actionSubmit );
+					},
+					actionSubmit: function( response ) {
+						$( '.nf-form-wrap form' )
+							.attr( 'target', 'ninja-forms-autocomplete' )
+							.attr( 'action', ajaxurl )
+							[0].submit();
+					},
+				});
+				new HcNfAutocomplete();
 		} );
-	</script>
-<?php
+		</script>
+		<?php
+	};
+
+	add_action( 'wp_footer', $print_js, 100 );
 }
-add_action( 'wp_footer', 'hcommons_ninja_forms_autocomplete', 100 );
+
+// Run only on nf_init so as not to embed JS where the plugin isn't active.
+add_action( 'nf_init', 'hcommons_ninja_forms_autocomplete' );
